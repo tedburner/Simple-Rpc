cc51103ed76b64ded6a9866caa9ad6a06e0c309a
arthas <jlj_vip98@qq.com>
2019-04-30 17:27:13 +0800

    添加zookeeper实现服务的注册和发现（未完成）

--- rpc-registry-zookeeper/src/main/java/com/simple/registry/zookeeper/registry/ZooKeeperServiceRegistry.java
+++ rpc-registry-zookeeper/src/main/java/com/simple/registry/zookeeper/registry/ZooKeeperServiceRegistry.java
@@ -1,7 +1,16 @@
 package com.simple.registry.zookeeper.registry;
 
+import com.simple.registry.zookeeper.constant.ZkConstants;
 import com.simple.rpc.registry.ServiceRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.I0Itec.zkclient.ZkClient;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author: lingjun.jlj
@@ -11,8 +20,45 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ZooKeeperServiceRegistry implements ServiceRegistry {
 
+    private ZkClient zkClient;
+    private static CountDownLatch latch = new CountDownLatch(1);
+
+    public ZooKeeperServiceRegistry(String zkAddress) {
+        // 创建 ZooKeeper 客户端
+        zkClient = new ZkClient(zkAddress, ZkConstants.SESSION_TIMEOUT, ZkConstants.CONNECTION_TIMEOUT);
+        log.info("connect zookeeper");
+    }
+
     @Override
     public void register(String serviceName, String serviceAddress) {
 
+        try {
+            String registryPath = ZkConstants.REGISTRY_PATH;
+            if (zkClient.exists(registryPath)) {
+                zkClient.createPersistent(registryPath);
+                log.info("zk create registry node: {}", registryPath);
+            }
+            //创建服务节点（持久化）
+            String servicePath = registryPath + "/" + serviceName;
+            if (zkClient.exists(servicePath)) {
+                zkClient.createPersistent(servicePath);
+                log.info("zk create service node: {}", servicePath);
+            }
+            //创建 address 节点（临时）
+            String addressPath = servicePath + "/address-";
+            String addressNode = zkClient.createEphemeralSequential(addressPath, serviceAddress);
+            log.info("zk create ip address node: {}",addressNode);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("zk create error: {}", e.getMessage());
+        }
+
     }
+
+//    @Override
+//    public void process(WatchedEvent watchedEvent) {
+//        if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
+//            latch.countDown();
+//        }
+//    }
 }
